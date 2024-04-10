45977d0ed4755a8c133f2bd42dd7ae4b7e0a7e26
vicky898 <maurya.8@iitj.ac.in>
2024-04-11 01:21:19 +0530

    Update settings.py

--- shop_project/shop_project/settings.py
+++ shop_project/shop_project/settings.py
@@ -172,8 +172,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import django_heroku
-django_heroku.settings(locals())
+import os
+STATIC_ROOT = os.path.join(BASE_DIR ,"staticfiles")
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
